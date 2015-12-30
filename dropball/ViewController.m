8968e53678b0325b87481f355762cdc237983030
Apple <Apple@Apples-MacBook-Air.local>
2015-12-30 23:14:24 +0700

    first commit

--- dropball/ViewController.m
+++ dropball/ViewController.m
@@ -12,11 +12,56 @@
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    UIImageView *ball;
+    float radius,velocity,accelarator,maxheight;
+    NSTimer *timer;
+}
+
+- (void) initproject{
+    radius = 64;
+    velocity= 0;
+    accelarator =9.8;
+    maxheight = self.view.bounds.size.height - radius;
+
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    // delete the upside part
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self initproject];
+    [self ballcreating];
+    [self balldropping];
+    
+}
+
+- (void) ballcreating{
+    ball = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"ball.png"]];
+    ball.frame = CGRectMake(0, 0, 128, 128);
+    ball.center = CGPointMake(self.view.bounds.size.width/2,radius);
+    [self.view addSubview:ball];
+    
+}
+- (void) balldropping{
+    timer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(doballdropping) userInfo:nil repeats:true];
+    
+}
+- (void) doballdropping{
+    ball.center = CGPointMake(ball.center.x, ball.center.y + [self goingdistance]);
+    
+    if (ball.center.y>=self.view.bounds.size.height-radius){
+        velocity = -velocity;
+        velocity = velocity * 0.9;
+        ball.center = CGPointMake(ball.center.x, self.view.bounds.size.height - radius);
+    }
+  
+    
+    
+}
+- (float) goingdistance{
+    velocity = velocity + accelarator*0.01;
+    return  velocity*0.01 *500; // it may have to multiple more but just 500
 }
 
 - (void)didReceiveMemoryWarning {
